aa912f239a41d3df83aa2c45924bc1183c342416
Watson <watson1978@gmail.com>
2018-08-26 02:31:03 +0900

    Update Electron to v3.0.0

--- script/linux/build.sh
+++ script/linux/build.sh
@@ -7,7 +7,7 @@ rm -rf ./out/build
 ./script/build-js.sh
 
 # electron requires electron-vX.Y-linux-x64 of sqlite3
-cp -a ./node_modules/sqlite3/lib/binding/electron-v2.0-linux-x64 ./out/js/node_modules/sqlite3/lib/binding/
+cp -a ./node_modules/sqlite3/lib/binding/electron-v3.0-linux-x64 ./out/js/node_modules/sqlite3/lib/binding/
 
 # build app with electron-packager
 VERSION=$(grep version package.json | head -n 1 | cut -f 2 -d : | \sed 's/[",]//g')
