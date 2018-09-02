78159f783736bf4a64556272e6f0ee58e6dc431c
h13i32maru <h13i32maru@gmail.com>
2018-09-02 11:31:32 +0900

    chore(package): update packages

--- script/mac/build-sqlite3.sh
+++ script/mac/build-sqlite3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ./node_modules/sqlite3
-npm i nan@2.3.3
-npm i node-pre-gyp@0.6.28
-npm i -g node-gyp@3.3.1
+npm i nan@2.10.0
+npm i node-pre-gyp@0.10.1
+npm i -g node-gyp@3.8.0
 node-gyp configure  --module_name=node_sqlite3 --module_path=../lib/binding/electron-v2.0-darwin-x64
 node-gyp rebuild  --target=2.0.8 --arch=x64 --target_platform=darwin --dist-url=https://atom.io/download/electron --module_name=node_sqlite3 --module_path=../lib/binding/electron-v2.0-darwin-x64
