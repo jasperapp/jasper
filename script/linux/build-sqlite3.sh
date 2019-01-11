6ec6548a9ff8f89c3b740a7276e646c822227fc8
Watson <watson1978@gmail.com>
2019-01-11 14:26:40 +0900

    Update Electron to v3.1.0

--- script/linux/build-sqlite3.sh
+++ script/linux/build-sqlite3.sh
@@ -3,5 +3,5 @@ cd ./node_modules/sqlite3
 npm i nan@2.10.0
 npm i node-pre-gyp@0.10.1
 npm i -g node-gyp@3.8.0
-node-gyp configure --module_name=node_sqlite3 --module_path=../lib/binding/electron-v3.0-linux-x64
-node-gyp rebuild --target=3.0.14 --arch=x64 --target_platform=linux --dist-url=https://atom.io/download/electron --module_name=node_sqlite3 --module_path=../lib/binding/electron-v3.0-linux-x64
+node-gyp configure --module_name=node_sqlite3 --module_path=../lib/binding/electron-v3.1-linux-x64
+node-gyp rebuild --target=3.1.0 --arch=x64 --target_platform=linux --dist-url=https://atom.io/download/electron --module_name=node_sqlite3 --module_path=../lib/binding/electron-v3.1-linux-x64
