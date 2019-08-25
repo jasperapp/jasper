96b4d46ffe2b42b5da3e5afb049b3032a80c637b
Watson <watson1978@gmail.com>
2019-08-25 16:29:11 +0900

    Update Electron to v6.0.3

--- script/mac/build-sqlite3.sh
+++ script/mac/build-sqlite3.sh
@@ -3,5 +3,5 @@ cd ./node_modules/sqlite3
 npm i nan@2.10.0
 npm i node-pre-gyp@0.10.1
 npm i -g node-gyp@3.8.0
-node-gyp configure  --module_name=node_sqlite3 --module_path=../lib/binding/electron-v4.0-darwin-x64
-node-gyp rebuild  --target=4.0.4 --arch=x64 --target_platform=darwin --dist-url=https://atom.io/download/electron --module_name=node_sqlite3 --module_path=../lib/binding/electron-v4.0-darwin-x64
+node-gyp configure  --module_name=node_sqlite3 --module_path=../lib/binding/electron-v6.0-darwin-x64
+node-gyp rebuild  --target=6.0.3 --arch=x64 --target_platform=darwin --dist-url=https://atom.io/download/electron --module_name=node_sqlite3 --module_path=../lib/binding/electron-v6.0-darwin-x64
