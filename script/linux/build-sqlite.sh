35045806ded672354572dfe0af6be879ea52898f
Watson <watson1978@gmail.com>
2019-10-13 03:17:59 +0900

    Use installed Electron version

--- script/linux/build-sqlite.sh
+++ script/linux/build-sqlite.sh
@@ -4,4 +4,4 @@ npm i nan@2.14.0
 npm i node-pre-gyp@0.10.1
 npm i -g node-gyp@3.8.0
 node-gyp configure --module_name=node_sqlite3 --module_path=../lib/binding/electron-v6.0-linux-x64
-node-gyp rebuild --target=6.0.3 --arch=x64 --target_platform=linux --dist-url=https://atom.io/download/electron --module_name=node_sqlite3 --module_path=../lib/binding/electron-v6.0-linux-x64
+node-gyp rebuild --target=6.0.7 --arch=x64 --target_platform=linux --dist-url=https://atom.io/download/electron --module_name=node_sqlite3 --module_path=../lib/binding/electron-v6.0-linux-x64
