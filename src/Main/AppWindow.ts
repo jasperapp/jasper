f5df64a7fb11f0dc459982e3075cf80420044c98
h13i32maru <h13i32maru@gmail.com>
2020-07-23 12:54:31 +0900

    refactor(AppWindow):

--- src/Main/AppWindow.ts
+++ src/Main/AppWindow.ts
@@ -1,14 +1,48 @@
-import BrowserWindow = Electron.BrowserWindow;
+import {BrowserWindow, BrowserWindowConstructorOptions, screen} from 'electron';
+import windowStateKeeper from 'electron-window-state';
+import {Platform} from '../Util/Platform';
 
 class _AppWindow {
-  private mainWindow: BrowserWindow;
+  private appWindow: BrowserWindow;
 
-  setWindow(mainWindow: BrowserWindow) {
-    this.mainWindow = mainWindow;
+  async init() {
+    const {width, height} = screen.getPrimaryDisplay().workAreaSize;
+    const mainWindowState = windowStateKeeper({
+      defaultWidth: Math.min(width, 1680),
+      defaultHeight: Math.min(height, 1027),
+    });
+
+    const config: BrowserWindowConstructorOptions = {
+      title: 'Jasper',
+      webPreferences: {
+        nodeIntegration: true
+      },
+      x: mainWindowState.x || 0,
+      y: mainWindowState.y || 0,
+      width: mainWindowState.width,
+      height: mainWindowState.height,
+    };
+
+    if (Platform.isLinux()) config.icon = `${__dirname}/../Electron/image/icon.png`;
+
+    const mainWindow = new BrowserWindow(config);
+
+    // 複数のディスプレイを使っている場合、ウィンドウの位置/サイズのリストアが不安定
+    // e.g. メインディスプレイより大きなサイズや、サブディスプレイに表示している場合など
+    // なので、作成したあとに再度サイズを設定し直す
+    // 多分electronの不具合
+    mainWindow.setPosition(mainWindowState.x || 0, mainWindowState.y || 0, false);
+    mainWindow.setSize(mainWindowState.width, mainWindowState.height)
+    mainWindowState.manage(mainWindow);
+
+    // prevent external web page
+    mainWindow.webContents.on('will-navigate', (ev, _url)=> ev.preventDefault());
+
+    this.appWindow = mainWindow;
   }
 
   getWindow() {
-    return this.mainWindow;
+    return this.appWindow;
   }
 }
 
