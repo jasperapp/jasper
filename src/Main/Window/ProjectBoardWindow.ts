bd5b03829c4d2eca0c09d46b898fc5cde70b0af7
h13i32maru <h13i32maru@gmail.com>
2020-09-06 14:02:18 +0900

    fix(ProjectBoardWindow): max size;

--- src/Main/Window/ProjectBoardWindow.ts
+++ src/Main/Window/ProjectBoardWindow.ts
@@ -1,4 +1,4 @@
-import {BrowserWindow} from 'electron';
+import {BrowserWindow, screen} from 'electron';
 import {AppWindow} from './AppWindow';
 import {ProjectBoardWindowIPC} from '../../IPC/ProjectBoardWindowIPC';
 
@@ -35,10 +35,10 @@ class _ProjectBoardWindow {
   private createWindow() {
     this.window = new BrowserWindow();
 
-    const rect1 = this.getCenterOnMainWindow(0.6);
+    const offsetX = 220; // 220px is SideColumn width
+    const rect1 = this.getCenterOnMainWindow(0.6, offsetX);
+    const rect2 = this.getCenterOnMainWindow(1, offsetX);
     this.window.setBounds(rect1);
-
-    const rect2 = this.getCenterOnMainWindow(1);
     this.window.setBounds(rect2, true);
 
     this.window.on('close', (_ev) => {
@@ -48,14 +48,20 @@ class _ProjectBoardWindow {
     this.window.webContents.addListener('console-message', (_ev, level, message) => ProjectBoardWindowIPC.eventConsoleMessage(level, message));
   }
 
-  private getCenterOnMainWindow(scale: number): {width: number, height: number, x: number, y: number} {
+  private getCenterOnMainWindow(scale: number, offsetX: number): {width: number, height: number, x: number, y: number} {
     const mainWindow = AppWindow.getWindow();
     const mainWindowSize = mainWindow.getSize();
     const mainWindowPos = mainWindow.getPosition();
-    const width = Math.floor(mainWindowSize[0] * scale);
+    let width = Math.floor(mainWindowSize[0] * scale);
     const height = Math.floor(mainWindowSize[1] * scale);
-    const x = Math.floor(mainWindowPos[0] + (mainWindowSize[0] / 2 - width / 2));
+    const x = offsetX + Math.floor(mainWindowPos[0] + (mainWindowSize[0] / 2 - width / 2));
     const y = Math.floor(mainWindowPos[1] + (mainWindowSize[1] / 2 - height / 2));
+
+    const display = screen.getDisplayNearestPoint({x, y});
+    if (width + x > display.workAreaSize.width) {
+      width = display.workAreaSize.width - x;
+    }
+
     return {width, height, x, y};
   }
 
