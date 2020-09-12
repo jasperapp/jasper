da5d1b57fe6f8b30cb7b24ad708237921b47d724
h13i32maru <h13i32maru@gmail.com>
2020-09-12 13:49:46 +0900

    refactor(appTheme):

--- src/Renderer/Library/Style/appTheme.ts
+++ src/Renderer/Library/Style/appTheme.ts
@@ -1,7 +1,6 @@
 import {ThemeNameEntity} from '../Type/ThemeNameEntity';
 
 const lightTheme = {
-  // bg
   bg: {
     primary: '#ffffff',
     primarySoft: '#eeeeee',
@@ -9,43 +8,55 @@ const lightTheme = {
     secondary: '#F5F5F4',
     third: '#F1F3F4',
   },
+  tab: {
+    bg: '#FCFCFC',
+    boxShadow: '1px 1px 0px 0px #44444429',
+    active: '#ffffff',
+  },
+  border: {
+    normal: '#dddddd',
+    bold: '#888888',
+  },
+  text: {
+    normal: '#444444',
+    soft: '#666666',
+    tiny: '#888888',
+  },
+  icon: {
+    normal: '#737475',
+    soft: '#888888',
+  },
+  button: {
+    normal: {
+      bg: '#FAFBFC',
+      border: '#D9DBDB',
+    },
+    primary: {
+      bg: '#2488FF',
+      border: '#238AF7',
+    }
+  },
+  iconButton: {
+    hover: '#00000011',
+  },
   // bg: '#ffffff',
   // bgSide: '#F5F5F4',
   // // bgSide: '#E6E9ED',
   // bgSideSelect: '#DCDFE1',
   // bgHover: '#DCDFE188',
   // bgSoft: '#eeeeee',
-  tab: {
-    bg: '#FCFCFC',
-    boxShadow: '1px 1px 0px 0px #44444429',
-    active: '#ffffff',
-  },
   // bgHover: '#cbdae5',
 
   // border
-  border: {
-    normal: '#dddddd',
-    bold: '#888888',
-  },
   // borderColor: '#dddddd',
   // borderBold: '#888888',
 
   // text
-  text: {
-    normal: '#444444',
-    soft: '#666666',
-    tiny: '#888888',
-  },
   // textColor: '#444444',
   // textSoftColor: '#666666',
   // textTinyColor: '#888888',
 
   // icon
-  icon: {
-    normal: '#737475',
-    soft: '#888888',
-    // tiny: '#aaaaaa',
-  },
   // iconColor: '#737475',
   // iconSoftColor: '#888888',
   // iconTinyColor: '#aaaaaa',
@@ -54,25 +65,6 @@ const lightTheme = {
   // textInputReadOnly: '#eeeeee',
 
   // button
-  button: {
-    normal: {
-      // bg: 'linear-gradient(to bottom, #fcfcfc 0%, #f1f1f1 100%)',
-      // border: '#c2c0c2 #c2c0c2 #a19fa1 #c2c0c2',
-      bg: '#FAFBFC',
-      border: '#D9DBDB',
-    },
-    primary: {
-      // bg: 'linear-gradient(to bottom, #6eb4f7 0%, #1a82fb 100%)',
-      // border: '#388df8',
-      bg: '#2488FF',
-      border: '#238AF7',
-    }
-  },
-
-  // icon button
-  iconButton: {
-    hover: '#00000011',
-  },
 
   // context menu
   // contextMenuColor: '#F5F5F4',
