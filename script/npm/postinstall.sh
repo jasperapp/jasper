d8aa97615312fab7b207c155ffba65e08c95e6c1
h13i32maru <h13i32maru@gmail.com>
2020-09-13 08:31:45 +0900

    refactor(mdi):

--- script/npm/postinstall.sh
+++ script/npm/postinstall.sh
@@ -7,4 +7,12 @@ rm -rf node_modules/@types/react-native
 # darkreader.jsをファイルとして読み込みたいのでコピーする
 cp -f node_modules/darkreader/darkreader.js src/Renderer/Fragment/Browser/BrowserFragmentAsset/
 
+# material design icon
+mkdir -p src/Renderer/asset/css
+mkdir -p src/Renderer/asset/fonts
+cp -f node_modules/@mdi/font/materialdesignicons.css src/Renderer/asset/css/
+cp -f node_modules/@mdi/font/materialdesignicons.css.map src/Renderer/asset/css/
+cp -f node_modules/@mdi/font/materialdesignicons.css.map src/Renderer/asset/css/
+cp -f node_modules/@mdi/font/fonts/* src/Renderer/asset/fonts/
+
 # todo mdiのcssファイルなどもコピーする
