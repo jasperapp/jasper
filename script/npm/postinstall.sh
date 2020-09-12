5f9cfd61e09a2ef60efd3507a6540b392f40ede7
h13i32maru <h13i32maru@gmail.com>
2020-09-13 08:35:02 +0900

    refactor(mdi):

--- script/npm/postinstall.sh
+++ script/npm/postinstall.sh
@@ -10,9 +10,8 @@ cp -f node_modules/darkreader/darkreader.js src/Renderer/Fragment/Browser/Browse
 # material design icon
 mkdir -p src/Renderer/asset/css
 mkdir -p src/Renderer/asset/fonts
-cp -f node_modules/@mdi/font/materialdesignicons.css src/Renderer/asset/css/
-cp -f node_modules/@mdi/font/materialdesignicons.css.map src/Renderer/asset/css/
-cp -f node_modules/@mdi/font/materialdesignicons.css.map src/Renderer/asset/css/
+cp -f node_modules/@mdi/font/css/materialdesignicons.css src/Renderer/asset/css/
+cp -f node_modules/@mdi/font/css/materialdesignicons.css.map src/Renderer/asset/css/
 cp -f node_modules/@mdi/font/fonts/* src/Renderer/asset/fonts/
 
 # todo mdiのcssファイルなどもコピーする
