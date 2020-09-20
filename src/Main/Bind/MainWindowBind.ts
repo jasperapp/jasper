c88ef665140a56a5b7ebd528de3a0720847b715d
h13i32maru <h13i32maru@gmail.com>
2020-09-20 09:22:41 +0900

    refactor(MainWindowBind):

--- src/Main/Bind/MainWindowBind.ts
+++ src/Main/Bind/MainWindowBind.ts
@@ -4,31 +4,35 @@ import {AppIPC} from '../../IPC/AppIPC';
 import {AppMenu} from '../Window/AppMenu';
 
 class _MainWindowBind {
+  private window: BrowserWindow;
+
   async bindIPC(window: BrowserWindow) {
+    this.window = window;
     AppIPC.initWindow(window);
 
     AppIPC.onReload(async () => window.webContents.reload());
     AppIPC.onIsSystemDarkTheme(() => nativeTheme.shouldUseDarkColors);
-    AppIPC.onToggleMaximizeWindow(async () => {
-      if (window.isMaximized()) {
-        window.unmaximize();
-      } else {
-        window.maximize();
-      }
-    });
-
-    AppIPC.onOpenNewWindow(async (_ev, url) => {
-      const p = new Promise(resolve => {
-        const window = MiscWindow.create(url);
-        window.on('close', () => resolve());
-      });
-      await p;
-    });
-
+    AppIPC.onToggleMaximizeWindow(async () => this.toggleMaximizeWindow());
+    AppIPC.onOpenNewWindow(async (_ev, url) => this.openNewWindow(url));
     AppIPC.onKeyboardShortcut((_ev, enable) => AppMenu.enableShortcut(enable));
-
     powerMonitor.on('suspend', () => AppIPC.powerMonitorSuspend());
     powerMonitor.on('resume', () => AppIPC.powerMonitorResume());
+  }
+
+  private async toggleMaximizeWindow() {
+    if (this.window.isMaximized()) {
+      this.window.unmaximize();
+    } else {
+      this.window.maximize();
+    }
+  }
+
+  private async openNewWindow(url: string) {
+    const p = new Promise(resolve => {
+      const window = MiscWindow.create(url);
+      window.on('close', () => resolve());
+    });
+    await p;
   }
 }
 
