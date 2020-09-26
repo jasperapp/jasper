f91c3837ddefbbb374133e5602e65c4df5305dcf
h13i32maru <h13i32maru@gmail.com>
2020-09-26 11:41:52 +0900

    feat(GitHubUserClient): limit max page

--- src/Renderer/Library/GitHub/GitHubUserClient.ts
+++ src/Renderer/Library/GitHub/GitHubUserClient.ts
@@ -5,7 +5,7 @@ import {RemoteUserWatchingEntity} from '../Type/RemoteGitHubV3/RemoteUserWatchin
 import {RemoteGitHubHeaderEntity} from '../Type/RemoteGitHubV3/RemoteGitHubHeaderEntity';
 
 export class GitHubUserClient extends GitHubClient {
-  // https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
+  // https://docs.github.com/en/free-pro-team@latest/rest/reference/users#get-the-authenticated-user
   async getUser(): Promise<{error?: Error; user?: RemoteUserEntity; githubHeader?: RemoteGitHubHeaderEntity; statusCode?: number}> {
     // note: アクセストークンのスコープ設定後にキャッシュから読み込ませないためにtをつけている
     const {error, body, githubHeader, statusCode} = await this.request<RemoteUserEntity>(`/user?t=${Date.now()}`);
@@ -14,16 +14,16 @@ export class GitHubUserClient extends GitHubClient {
     return {user: body, githubHeader};
   }
 
-  // https://docs.github.com/en/rest/reference/teams
-  async getUserTeams(page: number = 1): Promise<{error?: Error; teams?: RemoteUserTeamEntity[]}> {
+  // https://docs.github.com/en/free-pro-team@latest/rest/reference/teams
+  async getUserTeams(page: number = 1, maxPage: number = 10): Promise<{error?: Error; teams?: RemoteUserTeamEntity[]}> {
     const {error, body, headers} = await this.request<RemoteUserTeamEntity[]>('/user/teams', {per_page: 100, page});
     if (error) return {error};
 
     const teams: RemoteUserTeamEntity[] = body as RemoteUserTeamEntity[];
 
     const link = headers.get('link');
-    if (/page=\d+>; rel="next"/.test(link)) {
-      const res = await this.getUserTeams(page + 1);
+    if (/page=\d+>; rel="next"/.test(link) && page < maxPage) {
+      const res = await this.getUserTeams(page + 1, maxPage);
       if (res.teams) teams.push(...res.teams);
     }
 
@@ -37,16 +37,16 @@ export class GitHubUserClient extends GitHubClient {
     return {hasTeam: !!body.length};
   }
 
-  // https://docs.github.com/en/rest/reference/activity#list-repositories-watched-by-the-authenticated-user
-  async getUserWatchings(page: number = 1): Promise<{error?: Error; watchings?: RemoteUserWatchingEntity[]}> {
+  // https://docs.github.com/en/free-pro-team@latest/rest/reference/activity#list-repositories-watched-by-the-authenticated-user
+  async getUserWatchings(page: number = 1, maxPage: number = 10): Promise<{error?: Error; watchings?: RemoteUserWatchingEntity[]}> {
     const {error, headers, body} = await this.request<RemoteUserWatchingEntity[]>('/user/subscriptions', {per_page: 100, page});
     if (error) return {error};
 
     const watchings: RemoteUserWatchingEntity[] = body as RemoteUserWatchingEntity[];
 
     const link = headers.get('link');
-    if (/page=\d+>; rel="next"/.test(link)) {
-      const res = await this.getUserWatchings(page + 1);
+    if (/page=\d+>; rel="next"/.test(link) && page < maxPage) {
+      const res = await this.getUserWatchings(page + 1, maxPage);
       if (res.watchings) watchings.push(...res.watchings);
     }
 
