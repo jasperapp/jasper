cf50767a8851e1f04872e10a0484ebacd29c56aa
Watson <watson1978@gmail.com>
2020-12-31 15:14:35 +0900

    Introduce ARCH environment variable

--- script/env/setup-sqlite.sh
+++ script/env/setup-sqlite.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # sqlite3をビルドするにはDEVELOP.mdをみて環境を整える必要がある
-npx electron-rebuild -f -w sqlite3
+if [ -z $ARCH ]; then
+    ARCH=`uname -m`
+    if [ $ARCH = "x86_64" ]; then
+        ARCH="x64"
+    fi
+fi
+npx electron-rebuild -a $ARCH -f -w sqlite3
