f16f53ac03f248293da3f601f92b1d05314f69f4
h13i32maru <h13i32maru@gmail.com>
2022-03-27 09:40:32 +0900

    refactor: nits

--- src/Renderer/Library/GitHub/V4/GitHubV4ProjectNextClient.ts
+++ src/Renderer/Library/GitHub/V4/GitHubV4ProjectNextClient.ts
@@ -1,34 +1,11 @@
 import {GitHubV4Client} from './GitHubV4Client';
-import {RemoteGitHubV4Entity} from '../../Type/RemoteGitHubV4/RemoteGitHubV4Entity';
-
-type Field = {
-  name: string;
-  settings: string;
-  dataType: 'TITLE' | 'SINGLE_SELECT' | 'ITERATION' | 'TEXT' | 'NUMBER' | 'DATE' | string;
-}
-
-type RemoteGitHubV4ProjectNext = {
-  user?: {
-    projectNext?: {
-      fields: {
-        nodes: Field[]
-      }
-    }
-  }
-  organization?: {
-    projectNext?: {
-      fields: {
-        nodes: Field[]
-      }
-    }
-  }
-} & RemoteGitHubV4Entity;
+import {RemoteGitHubV4ProjectNextEntity, RemoteGitHubV4ProjectNextFieldEntity} from '../../Type/RemoteGitHubV4/RemoteGitHubV4ProjectNextEntity';
 
 export class GitHubV4ProjectNextClient extends GitHubV4Client {
   async getProjectStatusFieldNames(projectUrl: string): Promise<{ error?: Error; iterationName?: string; statusNames?: string[] }> {
     // fetch data
     const query = this.buildQuery(projectUrl);
-    const {error, data} = await this.request<RemoteGitHubV4ProjectNext>(query);
+    const {error, data} = await this.request<RemoteGitHubV4ProjectNextEntity>(query);
     if (error != null) return {error};
 
     // iteration
@@ -47,23 +24,13 @@ export class GitHubV4ProjectNextClient extends GitHubV4Client {
   private buildQuery(projectUrl: string): string {
     const [, orgOrUser, userName, , projectNumber] = new URL(projectUrl).pathname.split('/');
     const queryType = orgOrUser === 'orgs' ? 'organization' : 'user';
-    return `
-${queryType}(login: "${userName}") {
-  projectNext(number: ${projectNumber}) {
-    title
-    fields(first: 100) {
-      nodes {
-        dataType
-        name
-        settings
-      }
-    }
-  }
-}
-`;
+    return queryTemplate
+      .replace('__QUERY_TYPE__', queryType)
+      .replace('__USER_NAME__', userName)
+      .replace('__PROJECT_NUMBER__', projectNumber);
   }
 
-  private getIterationField(data: RemoteGitHubV4ProjectNext): Field | null {
+  private getIterationField(data: RemoteGitHubV4ProjectNextEntity): RemoteGitHubV4ProjectNextFieldEntity | null {
     if (data.user?.projectNext != null) {
       return data.user.projectNext.fields.nodes.find(field => field.dataType === 'ITERATION');
 
@@ -72,7 +39,7 @@ ${queryType}(login: "${userName}") {
     }
   }
 
-  private getStatusField(data: RemoteGitHubV4ProjectNext): Field | null {
+  private getStatusField(data: RemoteGitHubV4ProjectNextEntity): RemoteGitHubV4ProjectNextFieldEntity | null {
     if (data.user?.projectNext != null) {
       return data.user.projectNext.fields.nodes.find(field => {
         return field.dataType === 'SINGLE_SELECT' && field.name.toLocaleLowerCase() === 'status' && field.settings != null;
@@ -85,3 +52,18 @@ ${queryType}(login: "${userName}") {
     }
   }
 }
+
+const queryTemplate = `
+__QUERY_TYPE__(login: "__USER_NAME__") {
+  projectNext(number: __PROJECT_NUMBER__) {
+    title
+    fields(first: 100) {
+      nodes {
+        dataType
+        name
+        settings
+      }
+    }
+  }
+}
+`;
