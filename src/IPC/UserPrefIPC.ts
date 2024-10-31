9c7fca9ea2e9ea733fd56471e0ad63c9b3a46c41
sai keerthy kakarla <kakarlas@blackduck.com>
2024-10-31 15:28:04 -0400

    test

--- src/IPC/UserPrefIPC.ts
+++ src/IPC/UserPrefIPC.ts
@@ -1,4 +1,4 @@
-import {ipcMain, ipcRenderer} from 'electron';
+import { ipcMain, ipcRenderer } from 'electron';
 
 enum Channels {
   read = 'UserPrefIPC:read',
@@ -9,49 +9,57 @@ enum Channels {
 }
 
 class _UserPrefIPC {
+  private async invoke<T>(channel: Channels, ...args: any[]): Promise<T> {
+    return ipcRenderer.invoke(channel, ...args);
+  }
+
+  private handle<T>(channel: Channels, handler: (...args: any[]) => Promise<T>) {
+    ipcMain.handle(channel, (_ev, ...args) => handler(...args));
+  }
+
   // read
   async read(): Promise<string> {
-    return ipcRenderer.invoke(Channels.read);
+    return this.invoke<string>(Channels.read);
   }
 
   onRead(handler: () => Promise<string>) {
-    ipcMain.handle(Channels.read, handler);
+    this.handle(Channels.read, handler);
   }
 
   // write
   async write(text: string): Promise<void> {
-    return ipcRenderer.invoke(Channels.write, text);
+    return this.invoke<void>(Channels.write, text);
   }
 
   onWrite(handler: (text: string) => Promise<void>) {
-    ipcMain.handle(Channels.write, (_ev, text) => handler(text));
+    this.handle(Channels.write, handler);
   }
 
   // delete relative file
   async deleteRelativeFile(relativeFilePath: string): Promise<void> {
-    return ipcRenderer.invoke(Channels.deleteRelativeFile, relativeFilePath);
+    return this.invoke<void>(Channels.deleteRelativeFile, relativeFilePath);
   }
 
   onDeleteRelativeFile(handler: (relativeFilePath: string) => Promise<void>) {
-    ipcMain.handle(Channels.deleteRelativeFile, (_ev, relativeFilePath) => handler(relativeFilePath));
+    this.handle(Channels.deleteRelativeFile, handler);
   }
 
   // absolute file
   async getAbsoluteFilePath(relativeFilePath: string): Promise<string> {
-    return ipcRenderer.invoke(Channels.absoluteFilePath, relativeFilePath);
+    return this.invoke<string>(Channels.absoluteFilePath, relativeFilePath);
   }
 
   onGetAbsoluteFilePath(handler: (relativeFilePath: string) => Promise<string>) {
-    ipcMain.handle(Channels.absoluteFilePath, (_ev, relativeFilePath) => handler(relativeFilePath));
+    this.handle(Channels.absoluteFilePath, handler);
   }
 
   // each paths
-  async getEachPaths(): Promise<{userDataPath: string; userPrefPath: string}> {
-    return ipcRenderer.invoke(Channels.eachPaths);
+  async getEachPaths(): Promise<{ userDataPath: string; userPrefPath: string }> {
+    return this.invoke<{ userDataPath: string; userPrefPath: string }>(Channels.eachPaths);
   }
 
-  onGetEachPaths(handler: () => Promise<{userDataPath: string; userPrefPath: string}>) {
-    ipcMain.handle(Channels.eachPaths, (_ev) => handler());
+  onGetEachPaths(handler: () => Promise<{ userDataPath: string; userPrefPath: string }>) {
+    this.handle(Channels.eachPaths, handler);
   }
 }
 
