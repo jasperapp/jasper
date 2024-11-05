5c516f7ed3903ae47cf3e4d85fa33870a1b3a687
sai keerthy kakarla <kakarlas@blackduck.com>
2024-11-05 11:14:38 -0500

    insertion sort

--- src/Main/sort.ts
+++ src/Main/sort.ts
@@ -33,3 +33,22 @@ function merge(left: number[], right: number[]): number[] {
 const array = [38, 27, 43, 3, 9, 82, 10];
 const sortedArray = mergeSort(array);
 console.log(sortedArray); // Output: [3, 9, 10, 27, 38, 43, 82]
+
+
+// insertion sort
+function insertionSort(arr: number[]): number[] {
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && current < arr[j]) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // at this point, j is -1 or arr[j] < current
+        arr[j + 1] = current;
+    }
+
+    return arr;
+}
