cd92d6c63cda2606a6050f6d235877b1cce8bc5a
sai keerthy kakarla <kakarlas@blackduck.com>
2024-11-11 11:25:59 -0500

    test

--- script/copilot_written.py
+++ script/copilot_written.py
@@ -1,16 +1,29 @@
-def find_min(nums):
-    left, right = 0, len(nums) - 1
-    
-    while left < right:
-        mid = (left + right) // 2
-        
-        if nums[mid] > nums[right]:
-            left = mid + 1
-        else:
-            right = mid
-    
-    return nums[left]
+def max_product_subarray(nums):
+    if not nums:
+        return []
+
+    max_product = nums[0]
+    min_product = nums[0]
+    result = nums[0]
+    start = end = s = 0
+
+    for i in range(1, len(nums)):
+        if nums[i] < 0:
+            max_product, min_product = min_product, max_product
+
+        max_product = max(nums[i], max_product * nums[i])
+        min_product = min(nums[i], min_product * nums[i])
+
+        if max_product > result:
+            result = max_product
+            start = s
+            end = i
+
+        if max_product == nums[i]:
+            s = i
+
+    return nums[start:end + 1]
 
 # Example usage:
-nums = [4, 5, 6, 7, 0, 1, 2]
-print(find_min(nums))  # Output: 0
+nums = [2, 3, -2, 4]
+print(max_product_subarray(nums))  # Output: [2, 3]
