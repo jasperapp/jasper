ffe310f820f48342e2643b689653b69a80d7bd01
sai keerthy kakarla <kakarlas@blackduck.com>
2024-11-11 19:31:10 -0500

    test cases

--- src/Main/python/llm_keerthy_3.py
+++ src/Main/python/llm_keerthy_3.py
@@ -14,3 +14,47 @@ def searchMatrix(matrix, target):
             row += 1
 
     return False
+
+
+import unittest
+
+class TestSearchMatrix(unittest.TestCase):
+    def test_target_found(self):
+        matrix = [
+            [1, 4, 7, 11, 15],
+            [2, 5, 8, 12, 19],
+            [3, 6, 9, 16, 22],
+            [10, 13, 14, 17, 24],
+            [18, 21, 23, 26, 30]
+        ]
+        target = 5
+        self.assertTrue(searchMatrix(matrix, target))
+
+    def test_target_not_found(self):
+        matrix = [
+            [1, 4, 7, 11, 15],
+            [2, 5, 8, 12, 19],
+            [3, 6, 9, 16, 22],
+            [10, 13, 14, 17, 24],
+            [18, 21, 23, 26, 30]
+        ]
+        target = 20
+        self.assertFalse(searchMatrix(matrix, target))
+
+    def test_empty_matrix(self):
+        matrix = []
+        target = 1
+        self.assertFalse(searchMatrix(matrix, target))
+
+    def test_single_element_matrix_found(self):
+        matrix = [[1]]
+        target = 1
+        self.assertTrue(searchMatrix(matrix, target))
+
+    def test_single_element_matrix_not_found(self):
+        matrix = [[1]]
+        target = 2
+        self.assertFalse(searchMatrix(matrix, target))
+
+if __name__ == '__main__':
+    unittest.main()
