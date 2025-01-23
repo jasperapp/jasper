3bee993e557f95430c757f5932356133ef2c073c
sai keerthy kakarla <kakarlas@blackduck.com>
2025-01-23 12:59:53 -0500

    test

--- src/Main/python/sample_code.py
+++ src/Main/python/sample_code.py
@@ -24,3 +24,31 @@ arr = [1, 3, -1, -3, 5, 3, 6, 7]
 k = 3
 result = find_max_in_sliding_window(arr, k)
 print(result)  # Output: [3, 3, 5, 5, 6, 7]
+
+
+def find_min_in_sliding_window(arr, k):
+    if not arr or k <= 0:
+        return []
+    
+    n = len(arr)
+    if k > n:
+        return []
+
+    window = []
+    result = []
+    
+    for i in range(n):
+        if i >= k:
+            result.append(min(window))
+            window.pop(0)
+        window.append(arr[i])
+    
+    result.append(min(window))
+    
+    return result
+
+# Example Usage
+arr = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+result = find_min_in_sliding_window(arr, k)
+print(result)  # Output: [1, -1, -3, -3, 3, 3]
