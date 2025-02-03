98ba008e672fd5d0f6e6f0ff3e92eae251a1a215
sai keerthy kakarla <kakarlas@blackduck.com>
2025-02-03 11:42:48 -0500

    test

--- src/Main/python/flask_test.py
+++ src/Main/python/flask_test.py
@@ -61,3 +61,42 @@ def test_login_form_submission(self):
 
 def test_protected_page(self):
     app = Flask(__name__)
+
+
+def is_safe(board, row, col):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+def solve_n_queens_util(board, col):
+    if col >= len(board):
+        return True
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            if solve_n_queens_util(board, col + 1):
+                return True
+            board[i][col] = 0
+    return False
+
+def solve_n_queens(n):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    if not solve_n_queens_util(board, 0):
+        print("Solution does not exist")
+        return False
+    print_board(board)
+    return True
+
+def print_board(board):
+    for row in board:
+        print(" ".join(str(x) for x in row))
+
+# Example usage:
+solve_n_queens(4)
