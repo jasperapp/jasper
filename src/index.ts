de752f96d77a3ec5acf17eecbfe1a863d92b5b53
h13i32maru <h13i32maru@gmail.com>
2025-08-31 09:10:56 +0900

    feat(build): 開発用ビルドを整理

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import {app} from 'electron';
+import {app, nativeImage} from 'electron';
+import nodePath from 'node:path';
 import {browserViewIPCBind} from './IPC/BrowserViewIPC/BrowserViewIPC.bind';
 import {electronIPCBind} from './IPC/ElectronIPC/ElectronIPC.bind';
 import {mainWindowIPCBind} from './IPC/MainWindowIPC/MainWindowIPC.bind';
@@ -14,22 +15,24 @@ import {MainWindow} from './Main/Window/MainWindow/MainWindow';
 
 async function index() {
   await app.whenReady();
+
+  // 開発時のアイコンを設定
+  if (process.env.JASPER === 'DEV') {
+    const iconPath = nodePath.join(__dirname, 'Main/asset/image/jasper-dev.png');
+    app.dock.setIcon(nativeImage.createFromPath(iconPath));
+  }
+
+  // メインウィンドウを生成
   await MainWindow.init();
-
-  // bind IPC
   const window = MainWindow.getWindow();
-  // await MainWindowBind.bindIPC(window);
-  // await BrowserViewBind.bindIPC(window);
-  // await UserPrefBind.bindIPC(window);
-  // await SQLiteBind.bindIPC(window);
-  // await IssueBind.bindIPC(window);
-  // await StreamBind.bindIPC(window);
 
+  // 各種サービスを初期化
   MainWindowService.initWindow(window);
   BrowserViewService.initWindow(window);
   IssueService.initWindow(window);
   StreamService.initWindow(window);
 
+  // bind IPC
   mainWindowIPCBind(window);
   browserViewIPCBind();
   streamIPCBind();
